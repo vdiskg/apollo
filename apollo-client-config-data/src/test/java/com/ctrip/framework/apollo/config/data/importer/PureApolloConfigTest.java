60b12909d70480f7ad34cd4fd8f54193e64ff60c
vdisk <vdisk@foxmail.com>
2021-08-06 22:06:13 +0800

    add unit test

---
+++ apollo-client-config-data/src/test/java/com/ctrip/framework/apollo/config/data/importer/PureApolloConfigTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2021 Apollo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.ctrip.framework.apollo.config.data.importer;
+
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.build.ApolloInjector;
+import com.ctrip.framework.apollo.spi.DefaultConfigFactory;
+import com.ctrip.framework.apollo.util.ConfigUtil;
+import com.github.stefanbirkner.systemlambda.SystemLambda;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author vdisk <vdisk@foxmail.com>
+ */
+public class PureApolloConfigTest {
+
+  @Before
+  public void before() {
+    System.setProperty("env", "local");
+  }
+
+  @Test
+  public void testDefaultConfigWithSystemProperties() {
+    System.setProperty("spring.profiles.active", "test");
+    ConfigUtil configUtil = ApolloInjector.getInstance(ConfigUtil.class);
+    configUtil.setPureApolloConfig(false);
+    DefaultConfigFactory defaultConfigFactory = new DefaultConfigFactory();
+    Config config = defaultConfigFactory.create("application");
+    Assert.assertEquals(config.getProperty("spring.profiles.active", null), "test");
+  }
+
+  @Test
+  public void testPureApolloConfigWithSystemProperties() {
+    System.setProperty("spring.profiles.active", "test");
+    ConfigUtil configUtil = ApolloInjector.getInstance(ConfigUtil.class);
+    configUtil.setPureApolloConfig(true);
+    DefaultConfigFactory defaultConfigFactory = new DefaultConfigFactory();
+    Config config = defaultConfigFactory.create("application");
+    Assert.assertNull(config.getProperty("spring.profiles.active", null));
+  }
+
+  @Test
+  public void testDefaultConfigWithEnvironmentVariables() throws Exception {
+    SystemLambda.withEnvironmentVariable(
+        "SPRING_PROFILES_ACTIVE",
+        "test-env")
+        .execute(() -> {
+          ConfigUtil configUtil = ApolloInjector.getInstance(ConfigUtil.class);
+          configUtil.setPureApolloConfig(false);
+          DefaultConfigFactory defaultConfigFactory = new DefaultConfigFactory();
+          Config config = defaultConfigFactory.create("application");
+          Assert.assertEquals(config.getProperty("SPRING_PROFILES_ACTIVE", null), "test-env");
+        });
+  }
+
+  @Test
+  public void testPureApolloConfigWithEnvironmentVariables() throws Exception {
+    SystemLambda.withEnvironmentVariable(
+        "SPRING_PROFILES_ACTIVE",
+        "test-env")
+        .execute(() -> {
+          ConfigUtil configUtil = ApolloInjector.getInstance(ConfigUtil.class);
+          configUtil.setPureApolloConfig(true);
+          DefaultConfigFactory defaultConfigFactory = new DefaultConfigFactory();
+          Config config = defaultConfigFactory.create("application");
+          Assert.assertNull(config.getProperty("SPRING_PROFILES_ACTIVE", null));
+        });
+  }
+
+  @After
+  public void after() {
+    System.clearProperty("spring.profiles.active");
+    System.clearProperty("env");
+  }
+}
